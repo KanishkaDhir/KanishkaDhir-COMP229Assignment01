ac5bb5f6bf0ec01ee7f20f623887244fa5043f80
KanishkaDhir <97758881+KanishkaDhir@users.noreply.github.com>
2022-02-05 18:41:42 -0500

    final version

--- config/app.js
+++ config/app.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('../routes/index');
-//var usersRouter = require('../routes/users'); -- this is used in case another routes file is created
+
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
